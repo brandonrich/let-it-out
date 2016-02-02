9bbb949d42f57b63d31cff7aa8b06334c225ff17
Brandon Rich <brich@nd.edu1>
2016-02-01 21:56:05 -0500

    added cocoapods and firebase

---
+++ Pods/Target Support Files/Pods-Let it Out/Pods-Let it Out-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Let_it_Out : NSObject
+@end
+@implementation PodsDummy_Pods_Let_it_Out
+@end
---
+++ Pods/Target Support Files/Pods-Let it Out/Pods-Let it Out-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Let_it_OutVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Let_it_OutVersionString[];
+
